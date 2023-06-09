0691f1913d05b71a4ba79c2683f6659ccf1e6e20
Edoardo Baravaglio <edoardo.baravaglio@redergo.com>
2023-06-09 14:34:21 +0200

    project setup and first sections

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,34 @@
 'use client'
 
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
 import DarkModeToggle from './Buttons/DarkModeToggle';
 
 const Header = (): JSX.Element => {
-  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [darkMode, setDarkMode] = useState(prefersDarkMode);
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    if (darkMode) {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setDarkMode(prefersDarkMode);
+
+    if (prefersDarkMode) {
       document.body.classList.add('dark');
     } else {
       document.body.classList.remove('dark');
     }
-  }, [darkMode]);
+  }, []);
+
+  const handleDarkModeToggle = () => {
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+
+    if (newDarkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  };
 
   const logoSrc = darkMode ? '/img/logo-dark.svg' : '/img/logo-light.svg';
 
@@ -25,7 +38,7 @@ const Header = (): JSX.Element => {
         <Image src={logoSrc} width={200} height={100} className="max-w-[100px]" alt="TraceX" />
       </div>
       <div className="md:w-1/3 text-center flex center">
-        <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
+        <DarkModeToggle darkMode={darkMode} setDarkMode={handleDarkModeToggle} />
       </div>
       <div className="md:w-1/3 flex justify-end">
         <Link href="#trace" className="bold text-[30px] border-l-[1px] border-grey py-2 pl-3 md:pl-6 lg:pl-8">
